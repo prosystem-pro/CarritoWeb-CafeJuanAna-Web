a025766f73b1a9407edc1a511f95427f0cf89829
prosystem-pro <prosystem490@gmail.com>
2025-06-25 22:53:00 -0600

    Nombre Empresa actualizado

--- src/app/Entornos/Entorno.prod.ts
+++ src/app/Entornos/Entorno.prod.ts
@@ -2,5 +2,5 @@ export const Entorno = {
   Produccion: true,
   ApiUrl: 'https://carritoweb-cafejuanana-api.onrender.com/api/',
   // ApiUrl: 'https://carritoweb-promesadios-api-production.up.railway.app/api/',
-  NombreEmpresa: 'PanaderiaPromesaDeDios'
+  NombreEmpresa: 'CafeJuanAna'
 };
