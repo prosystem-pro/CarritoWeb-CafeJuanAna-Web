16b54c4fbeb92f28c5b229d8590a830509ed8f39
prosystem-pro <prosystem490@gmail.com>
2025-06-25 18:38:06 -0600

    Proyecto Cafe Juan Ana desplegado

---
+++ src/app/Entornos/Entorno.prod.ts
@@ -0,0 +1,6 @@
+export const Entorno = {
+  Produccion: true,
+  ApiUrl: 'https://carritoweb-cafejuanana-api.onrender.com/api/',
+  // ApiUrl: 'https://carritoweb-promesadios-api-production.up.railway.app/api/',
+  NombreEmpresa: 'PanaderiaPromesaDeDios'
+};
